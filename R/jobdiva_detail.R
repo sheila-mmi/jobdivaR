417109134b834e427507c5acbd38062a81b8d2d4
swarrick <sheila.warrick@itmmi.com>
2022-12-29 21:51:30 +0000

    Added ability to run bulk functions + corrected query

--- R/jobdiva_detail.R
+++ R/jobdiva_detail.R
@@ -10,26 +10,43 @@
 #' @return A dataframe of entity details for the given entities.
 #' @export
 
-jobdiva_detail = function(entity, id_entity, clean_entity_df)
+jobdiva_detail = function(entity, id_entity, clean_entity_df, bulk = TRUE)
 {
   # id_entity (string: what id are you using to identify 
   # (i.e. for companynotesdetail: entity = 'companynotes', id_entity = 'company')
   entity_ids = clean_entity_df$id
-  entity_ids = paste0(entity_ids, collapse = ',')
+  if (bulk == TRUE)
+  {
+    entity_name = paste0(id_entity, 'Ids')
+  }
+  
+  else
+  {
+    entity_name = paste0(id_entity, 'Id')
+  }
+  
+  entity_ids = split(entity_ids, ceiling(seq_along(entity_ids)/100))
+  entity_ids = lapply(entity_ids, function(x) {
+    x = paste0(x, collapse = paste0('&', entity_name, '='))
+    x =  paste0('?', entity_name, '=', x)
+  })
+
+  results = data.frame()
   
   base_url = "https://api.jobdiva.com/api/bi/"
   
   full_method = paste0(stringr::str_to_title(entity), "Detail")
   full_method = stringr::str_replace_all(full_method, " ", "")
-  request = httr::GET(url = paste0(base_url, full_method)
-                      , add_headers("Authorization" = jobdiva_login())
-                      , query = list(
-                        assign(paste0("'", tolower(id_entity), 'Id', "'"),  entity_ids)
-                        , 'alternateFormat' = TRUE
-                      )
-                      , encode = "json"
-                      , httr::verbose()) 
-  results = httr::content(request)
+  
+  for (i in 1:length(entity_ids))
+  {
+    request = httr::GET(url = paste0(base_url, full_method, entity_ids[[i]], '&alternateFormat=true', '&userFieldsName=PHX_Normalized_Company_Id')
+                        , add_headers("Authorization" = jobdiva_login())
+                        , encode = "json"
+                        , httr::verbose()) 
+    temp_results = dplyr::bind_rows(httr::content(request)[[2]])
+    results = rbind(results, temp_results)
+  }
   
   return(results)
 }
