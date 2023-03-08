2d9ba59966a6a7739c4bb7fd6cabcc95f02af990
swarrick <sheila.warrick@itmmi.com>
2023-03-08 20:06:38 +0000

    Create function to create a company in jobdiva

---
+++ R/jobdiva_create_company.R
@@ -0,0 +1,54 @@
+#' JobDiva Create Company
+#' 
+#' This function will create a company in JobDiva
+#' if it does not already exist.
+#' 
+#' @param company_name (type: string) -- a string indicating with the client id given by JobDiva
+#' @param company_website (type: string) -- email address of the designated API account
+#' @return The JobDiva company id of the created company
+#' @export
+
+
+jobdiva_create_company = function(company_name
+                                  , company_website)
+{
+  name = trimws(company_name)
+  name = str_replace_all(name, ' ', '%20')
+  
+  url = paste0('https://api.jobdiva.com/api/jobdiva/createCompany?companyname='
+               , name
+               , '&url='
+               , tolower(company_website))
+  
+  results = httr::POST(url
+                       , add_headers("Authorization" = jobdiva_login())
+                       , encode = "json"
+                       , httr::verbose())
+  
+  # Return Company ID
+  {
+    status = as.numeric(status_code(results))
+    
+    # 200 code: successful
+    # 500 code: company already exists
+    # anything else: ERROR
+    
+    if (status == 200)
+    {
+      cont = httr::content(results)
+      cont = as.numeric(cont)
+    }else if (status == 500)
+    {
+      cont = httr::content(results)
+      cont = cont$message
+      cont = stringr::str_extract(cont, regex("id\\:(.*)\\)"))
+      cont = substring(cont, 4,nchar(cont)-1)
+      
+    }else {
+      cont = character(0)
+    }
+  }
+  
+  return(cont)
+}
+
