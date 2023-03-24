36388a22b43f804f1fbe71fddd728dcc7a864fa1
swarrick <sheila.warrick@itmmi.com>
2023-03-24 17:34:12 +0000

    Changed output

--- R/jobdiva_get_emails.R
+++ R/jobdiva_get_emails.R
@@ -35,6 +35,7 @@ jobdiva_get_emails = function(entity, entity_id)
   if(class(results)[1] != 'try-error')
   {
     result_df = try(dplyr::bind_rows(results), silent = TRUE)
+    return(result_df)
   }
   
   return(results)
